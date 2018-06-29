54e937eeb1c7fe8336dc0f211e3670638a9fb802
aamzerin <Abdessamad@192.168.1.4>
2018-06-29 07:16:11 +0100

    added some changes and methods in Accounts and Operations

--- src/main/java/io/aamzerin/kata/bankaccount/service/AccountService.java
+++ src/main/java/io/aamzerin/kata/bankaccount/service/AccountService.java
@@ -1,8 +1,12 @@
 package io.aamzerin.kata.bankaccount.service;
 
+import java.math.BigDecimal;
+
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.aamzerin.kata.bankaccount.domain.Account;
 import io.aamzerin.kata.bankaccount.repository.AccountRepository;
 
 @Service
@@ -10,5 +14,18 @@ public class AccountService {
 	
 	@Autowired
 	AccountRepository accountRepository;
+	
+	@Autowired
+	OperationService operationService;
+	
+	@Test
+	public void makeAccountOperation(Account account,BigDecimal amount,Integer entryType) {
+		
+	}
+	
+	@Test
+	public void getAccountHistory(Account account) {
+		
+	}
 
 }
--- src/test/java/io/aamzerin/kata/bankaccount/service/AccountServiceTest.java
+++ src/test/java/io/aamzerin/kata/bankaccount/service/AccountServiceTest.java
@@ -1,12 +1,15 @@
 package io.aamzerin.kata.bankaccount.service;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
+import io.aamzerin.kata.bankaccount.constant.EntryType;
 import io.aamzerin.kata.bankaccount.domain.Account;
 import io.aamzerin.kata.bankaccount.domain.Client;
 import io.aamzerin.kata.bankaccount.domain.Operation;
@@ -16,37 +19,47 @@ public class AccountServiceTest {
 
 	@Mock
 	AccountRepository accountRepository;
-	
+
+	@Mock
+	OperationService operationService;
+
 	@InjectMocks
 	AccountRepository accountservice;
-	
+
 	private Client client;
 	private Account account;
 	private Operation operation;
-	
-	
-    @Before
-    public void setUp() {
 
-        client = Client.builder().clientId(1L).firstName("John").secondName("Doe").username("jdoe").build();
-        account = Account.builder().accountNumber("0500013M026").client(client).balance(new BigDecimal(100000)).build();
-        operation = Operation.builder().account(account).build();
-    }
-	
+	@Before
+	public void setUp() {
+
+		client = Client.builder().clientId(1L).firstName("John").secondName("Doe").username("jdoe").build();
+		account = Account.builder().accountNumber("0500013M026").client(client).balance(new BigDecimal(100000)).build();
+		operation = Operation.builder().account(account).build();
+	}
+
 	@Test
-	public void makeDepositinAccount() {
-		
-		
+	public void makeCreditOperation() {
+
+		Mockito.when(accountRepository.save(account)).thenReturn(account);
+		Mockito.when(operationService.createOperation(account, new BigDecimal(500L), EntryType.CREDIT.getEntryType()))
+				.thenReturn(new Operation(1l, new BigDecimal(500L), EntryType.CREDIT.getEntryType(), LocalDateTime.now(), account));
+
 	}
 	
 	@Test
-	public void makeWithdrawalinAccount() {
-		
+	public void makeDebitOperation() {
+
+		Mockito.when(accountRepository.save(account)).thenReturn(account);
+		Mockito.when(operationService.createOperation(account, new BigDecimal(500L), EntryType.CREDIT.getEntryType()))
+				.thenReturn(new Operation(1l, new BigDecimal(500L), EntryType.DEBIT.getEntryType(), LocalDateTime.now(), account));
+
 	}
-	
+
+
 	@Test
-	public void retrieveOperationsHistory() {
-		
+	public void getHistory(Account account) {
+
 	}
-	
+
 }
